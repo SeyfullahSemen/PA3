75f8bfac19ebd7f829486b919e357a4ed21ed7ec
Seyfullah <seyfullah.semen@hotmail.com>
2019-03-29 16:09:00 +0100

    Added the encrypt function to the project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.math.BigInteger;
 import java.util.Random;
@@ -16,19 +18,76 @@ public class Controller {
 
     // Dit is de zin die wij moeten encrypten
     private final String SENTENCE = "Good things come to people who wait, but better things come to those who go out and get them ~Anonymous";
-    //private final Long N_FOR_ENCRYPTION = 21691L; // De N die wij hebben gekregen van Anke
-    //private final int E_FOR_ENCRYPTION = 3; // De E die wij zelf hebben uitgekozen.
+    private String encryptedMessage = "";
     private static final Random random = new Random();
-    private static final BigInteger INIT_NUMBER = new BigInteger("21691");
+    private static final BigInteger INIT_NUMBER = new BigInteger("2");
     private static BigInteger p = INIT_NUMBER;
     private static BigInteger q;
     private static BigInteger e = new BigInteger("3");
     private static BigInteger modulus;
     private static BigInteger phi;
-    long startTime = (int) System.nanoTime();
 
+    private static BigInteger encoder(int message) {
+        BigInteger messageConversion = new BigInteger("" + message);
+        return messageConversion.modPow(e, modulus);
+    }
 
+    private static void init(String N) {
+        //Initialise n and p
+        BigInteger n = new BigInteger(N);
 
+        //For each prime p
+        while (p.compareTo(n.divide(INIT_NUMBER)) <= 0) {
 
+            //If we find p
+            if (n.mod(p).equals(BigInteger.ZERO)) {
+                //Calculate q
+                q = n.divide(p);
+                modulus = p.multiply(q);
+                phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+                return;
+                //The end of the algorithm
+            }
+            //p = the next prime number
+            p = p.nextProbablePrime();
+        }
+        System.out.println("There is no solution for this number.");
+    }
+
+    /**
+     * Encrypt the message we have been given
+     */
+    private void encryptMessage() {
+        long startTime = (int) System.nanoTime();
+        init("21691");
+
+        while (phi.gcd(e).intValue() > 1) {
+            e = e.add(new BigInteger("2"));
+        }
+
+        String encryptedMessage = "";
+        String[] sentenceArray = SENTENCE.split("");
+        for (int i = 0; i < sentenceArray.length; i++) {
+            for (int j = 0; j < sentenceArray[i].length(); j++) {
+                char character = sentenceArray[i].charAt(j);
+                int ascii = (int) character;
+                encryptedMessage += " " + encoder(ascii);
+            }
+        }
+        System.out.println(encryptedMessage);
+    }
+
+    /**
+     * Call the encrypt message after mouse click on the
+     * button to encrypt.
+     */
+    public void onEncryptButtonClick(){
+        encryptNowButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                encryptMessage();
+            }
+        });
+    }
 
 }
