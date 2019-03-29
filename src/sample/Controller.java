003ebe229d6cdd737a90393dbfda5d9d4ef50851
Seyfullah <seyfullah.semen@hotmail.com>
2019-03-29 15:42:31 +0100

    ADDED NEW CODE

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Controller {
     @FXML
     private Button encryptNowButton;
@@ -13,9 +16,17 @@ public class Controller {
 
     // Dit is de zin die wij moeten encrypten
     private final String SENTENCE = "Good things come to people who wait, but better things come to those who go out and get them ~Anonymous";
-    private final Long N_FOR_ENCRYPTION = 21691L; // De N die wij hebben gekregen van Anke
-    private final int E_FOR_ENCRYPTION = 3; // De E die wij zelf hebben uitgekozen.
-    
+    //private final Long N_FOR_ENCRYPTION = 21691L; // De N die wij hebben gekregen van Anke
+    //private final int E_FOR_ENCRYPTION = 3; // De E die wij zelf hebben uitgekozen.
+    private static final Random random = new Random();
+    private static final BigInteger INIT_NUMBER = new BigInteger("21691");
+    private static BigInteger p = INIT_NUMBER;
+    private static BigInteger q;
+    private static BigInteger e = new BigInteger("3");
+    private static BigInteger modulus;
+    private static BigInteger phi;
+    long startTime = (int) System.nanoTime();
+
 
 
 
